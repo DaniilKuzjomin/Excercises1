9b58acaa504b099c72f8eff7679927a23ee9b92b
opilane <opilane@HP-CZC0359NNZ>
2021-12-07 09:53:24 +0200

    Second task

--- PersonalityTest/Program.cs
+++ PersonalityTest/Program.cs
@@ -11,6 +11,27 @@ namespace PersonalityTest
             // 'red' - romantic, 'blue' - reliable, 'yellow' - cheerful
             //if the user enters anything else, the programm displays
             //"You are a [userInput] unicorn."
+
+
+            Console.WriteLine("Please, enter your favorite color: ");
+            string userInput = Console.ReadLine();
+
+            if ( userInput == "red" )
+            {
+                Console.WriteLine("You are a romantic unicorn. ");
+            }
+            else if ( userInput == "Blue" )
+            {
+                Console.WriteLine("You are a reliable unicorn. ");
+            }
+            else if ( userInput == "Yellow" )
+            {
+                Console.WriteLine("You are a cheerful unicorn. ");
+            }
+            else
+            {
+                Console.WriteLine($"Your are a {userInput} unicorn");
+            }
         }
     }
 }
