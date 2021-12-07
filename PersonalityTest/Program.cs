31a36f8d3b770db4e4ffa1d6a7dd1f2a95e7da01
opilane <opilane@HP-CZC0359NNZ>
2021-12-07 09:41:51 +0200

    First code

---
+++ PersonalityTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PersonalityTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //the program and the user to enter their favorite color
+            //the program displays the users personality traits
+            // 'red' - romantic, 'blue' - reliable, 'yellow' - cheerful
+            //if the user enters anything else, the programm displays
+            //"You are a [userInput] unicorn."
+        }
+    }
+}
